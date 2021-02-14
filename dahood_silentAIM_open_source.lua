8fa57ee62e6a2303bebd37e72713be5ad6a6e2e5
misamiru <71678108+misamiru@users.noreply.github.com>
2021-02-14 18:14:50 -0300

    Create dahood_silentAIM_open_source.lua
    silent aim/aimlock que hice para da hood, es codigo abierto asi que si al menos lo robas deja los creditos :(

---
+++ dahood_silentAIM_open_source.lua
@@ -0,0 +1,194 @@
+--[[
+
+
+                                  ███▄,                              ,╓╖╓,
+                                   ╙█████░▒▄'                      ▄███▒▓▓╣╣╢╗
+                                      ▀▀█████▄,:                  ▐█████████▓▓▓▄
+                                          ` █▓░  █╖               ████████▓▄▓▀██
+                                            '██▄▄█▓▒╗,             ███████▀▀▀██▀
+                                              ▀█████▓▒╢╖            ▀██▄▄░░░░sT
+                                                ██████▓╬╢╖          └███░░░░░─
+                                                 ▀█████▓╣╣▒╣         ███▄µ▒░░
+                                                   ▀████▓▓▓▓╫┐       ,██▀▀
+                                                     ████▓▓█▓▌@▓▒▓▒▄@▓█▓µ▌\  ┌╖
+                                                      ▐███████▓▓▓▓▓▒▄▌╢╢╣▀▒ ,░░░▒▒∩,,
+                                                       ▀███████▓█▓▓╩▒▀█▄▒▒▒▄▒░░░|░▒░░]▒
+                                                          ▀████████▓▓▓▒▀███Ñ▓▒░▒░░░░░▒▒▒
+                                                           ░`▀██████▓▓▓@▒███▒▒▒▒▀▒¢g▄╢▒░
+                                                              ▐█████▓▓▓▓╣▓▒██▌▒▒▒▒▒▒▐███▄
+                                                             ░ ██████▓▓▓▓▌Ñ▓▓▀██▄▒╢¼████æ▄
+                                                               ▀███████▓███▓▓▓▓█░░░░▀▀▀▀██▄
+                                                              ╒█████████▓██▓▓▓▓█▒▒▒▒▒▒▒▒▒░░▄
+                                                             ,▓█╣▓██▓▓███████▓▓█▒▒▒╢╢▒▒▒▒▒▒░
+                                                            ▄▓█▓▄▓███▓█████████████▄▒╢▓╣▓▓╜
+                                                           ▐████▓▓██▓▓▓▓▒▒╢╣╣▒▒▒▀█▀█-└▀▀`
+
+
+
+                                        ▄▄▄█████▓▓█████  ▄████▄   ██░ ██  █     █░ ▄▄▄       ██▀███  ▓█████ 
+                                        ▓  ██▒ ▓▒▓█   ▀ ▒██▀ ▀█  ▓██░ ██▒▓█░ █ ░█░▒████▄    ▓██ ▒ ██▒▓█   ▀ 
+                                        ▒ ▓██░ ▒░▒███   ▒▓█    ▄ ▒██▀▀██░▒█░ █ ░█ ▒██  ▀█▄  ▓██ ░▄█ ▒▒███   
+                                        ░ ▓██▓ ░ ▒▓█  ▄ ▒▓▓▄ ▄██▒░▓█ ░██ ░█░ █ ░█ ░██▄▄▄▄██ ▒██▀▀█▄  ▒▓█  ▄ 
+                                          ▒██▒ ░ ░▒████▒▒ ▓███▀ ░░▓█▒░██▓░░██▒██▓  ▓█   ▓██▒░██▓ ▒██▒░▒████▒
+                                          ▒ ░░   ░░ ▒░ ░░ ░▒ ▒  ░ ▒ ░░▒░▒░ ▓░▒ ▒   ▒▒   ▓▒█░░ ▒▓ ░▒▓░░░ ▒░ ░
+                                            ░     ░ ░  ░  ░  ▒    ▒ ░▒░ ░  ▒ ░ ░    ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ░  ░
+                                          ░         ░   ░         ░  ░░ ░  ░   ░    ░   ▒     ░░   ░    ░   
+                                                    ░  ░░ ░       ░  ░  ░    ░          ░  ░   ░        ░  ░
+                                                        ░                                                   
+
+                                         ▄▄▄▄ ▓██   ██▓    ███▄ ▄███▓ ██▓  ██████  ▄▄▄      ▓█████  ██▓    
+                                        ▓█████▄▒██  ██▒   ▓██▒▀█▀ ██▒▓██▒▒██    ▒ ▒████▄    ▓█   ▀ ▓██▒    
+                                        ▒██▒ ▄██▒██ ██░   ▓██    ▓██░▒██▒░ ▓██▄   ▒██  ▀█▄  ▒███   ▒██░    
+                                        ▒██░█▀  ░ ▐██▓░   ▒██    ▒██ ░██░  ▒   ██▒░██▄▄▄▄██ ▒▓█  ▄ ▒██░    
+                                        ░▓█  ▀█▓░ ██▒▓░   ▒██▒   ░██▒░██░▒██████▒▒ ▓█   ▓██▒░▒████▒░██████▒
+                                        ░▒▓███▀▒ ██▒▒▒    ░ ▒░   ░  ░░▓  ▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░░░ ▒░ ░░ ▒░▓  ░
+                                        ▒░▒   ░▓██ ░▒░    ░  ░      ░ ▒ ░░ ░▒  ░ ░  ▒   ▒▒ ░ ░ ░  ░░ ░ ▒  ░
+                                         ░    ░▒ ▒ ░░     ░      ░    ▒ ░░  ░  ░    ░   ▒      ░     ░ ░   
+                                         ░     ░ ░               ░    ░        ░        ░  ░   ░  ░    ░  ░
+                                              ░░ ░                                                         
+
+
+-- // INFORMACION:
+-- silent aim/aimlock que "predice" el movimiento del jugador
+-- solo funciona en synapse X o sirhurt (posiblemente krnl pero no probe)
+-- presiona la tecla1 para lockear a alguien
+-- presiona la tecla2 para deslockear a alguien
+-- presiona la tecla3 para desactivar/activar el lock
+
+-- // CREDITOS:
+-- hecho por cute_misael en roblox/misael#1051 (550687407020965888) en discord
+-- gracias a 2a60/Horbyss (tfw cuando eres pedofilo, te muestran pruebas y aun asi decis que no sos pedofilo plus modulador de voz) por la idea
+
+-- // CREDITOS ADICIONALES: 
+-- doxxeados_asesinos (mininos asesinos)
+-- 4PEDOFILO (4drizzy)
+-- 26Skid (tfw cuando alguien que no muchos conocen hace audios mas fuertes que los tuyos y le decis que usa metodos de otras personas y usas metodos de otros kek)
+-- Skidable19 (tfw cuando intentas cookie loggear)
+-- por ultimo gracias a pedos on top (best on top) y R (lleno de virgos)
+
+-- // NOTAS:
+-- fuck UserInputService all my homies use GetMouse
+-- esta Open Source si al menos lo usas dame creditos peteeee
+
+]]
+
+-- // servicios
+local DAHOODPLAYERS = game:GetService("Players")
+local thisaudiowasmadebytueniSEXdrip = game:GetService("StarterGui")
+
+-- // objetos
+local THISAUDIOWASMADEBYDRIZZY = DAHOODPLAYERS.LocalPlayer
+local mordida_del_87 = THISAUDIOWASMADEBYDRIZZY:GetMouse()
+
+-- // variables
+local doxxeados_asesinos = getrawmetatable(game)
+local dahood = doxxeados_asesinos.__namecall
+local drizzy_groomer = {
+  ["activado"] = true,
+  ["victima"] = nil,
+}
+
+-- // creditos (si de verdad eliminas/editas esto para poner tus creditos, sos la persona mas triste del mundo)
+thisaudiowasmadebytueniSEXdrip:SetCore("SendNotification",{
+    Title="TECHWARE 最好的代碼",
+    Text = "AIMLOCK ACTIVADO\nHECHO POR CUTE_MISAEL"
+})
+
+wait(1)
+
+thisaudiowasmadebytueniSEXdrip:SetCore("SendNotification",{
+    Title="TECHWARE 最好的代碼",
+    Text = "APRETA LA TECLA "..teclas.tecla3.." PARA ACTIVAR EL AIMLOCK"
+})
+thisaudiowasmadebytueniSEXdrip:SetCore("SendNotification",{
+    Title="TECHWARE 最好的代碼",
+    Text = "APRETA LA TECLA "..teclas.tecla2.." PARA DESLOCKEAR"
+})
+thisaudiowasmadebytueniSEXdrip:SetCore("SendNotification",{
+    Title="TECHWARE 最好的代碼",
+    Text = "APRETA LA TECLA "..teclas.tecla1.." PARA LOCKEAR A ALGUIEN\n(pon el mouse sobre la persona que quieres lockear)"
+})
+
+-- // funciones utiles
+function esautista(minino_asesino)
+    if DAHOODPLAYERS:GetPlayerFromCharacter(minino_asesino) then
+        return true
+    else
+        return false
+    end
+end
+
+function matar_y_violar_su_cuerpo_putrefacto_como_haria_kiIIuli(victima)
+    return DAHOODPLAYERS:GetPlayerFromCharacter(victima)
+end
+
+function groomear(pedofilo,victima,esmujer,esmenor)
+	if pedofilo == "Drizzy" and victima and esmujer and esmenor then
+		return victima.Character.Head.Position + victima.Character.UpperTorso.Velocity * 0.165
+	end
+end
+
+function espedofilo(jugadorDeDH,fdmg)
+	if jugadorDeDH == "Drizzy" then
+		if tostring(fdmg) == "MainEvent" then
+			return true
+		end
+	end
+end
+
+-- // conexion
+mordida_del_87.KeyDown:Connect(function(AMONGUS)
+    if AMONGUS:lower() == teclas.tecla1:lower() then
+        local genital = mordida_del_87.Target
+        if genital then
+            local victima_de_grooming = genital.Parent
+            if victima_de_grooming:IsA("Accessory") then
+                victima_de_grooming = victima_de_grooming.Parent
+            elseif victima_de_grooming.Name == "SpecialParts" then
+                victima_de_grooming = victima_de_grooming.Parent.Parent
+            end
+            if victima_de_grooming:FindFirstChild("Humanoid") and esautista(victima_de_grooming) then
+                drizzy_groomer.victima = matar_y_violar_su_cuerpo_putrefacto_como_haria_kiIIuli(victima_de_grooming)
+                thisaudiowasmadebytueniSEXdrip:SetCore("SendNotification",{
+                    Title = "TECHWARE 最好的代碼",
+                    Text = "aimlock activado en: "..victima_de_grooming.Name
+                })
+           end
+        end
+    elseif AMONGUS:lower() == teclas.tecla2:lower() then
+        if drizzy_groomer.victima then
+            local anterior_victima = drizzy_groomer.victima
+            drizzy_groomer.victima = nil
+            thisaudiowasmadebytueniSEXdrip:SetCore("SendNotification",{
+                Title = "TECHWARE 最好的代碼",
+                Text = "aimlock desactivado en: "..anterior_victima.Name
+            })
+        end
+    elseif AMONGUS:lower() == teclas.tecla3:lower() then
+        if drizzy_groomer.activado == true then
+            drizzy_groomer.activado = false
+            thisaudiowasmadebytueniSEXdrip:SetCore("SendNotification",{
+                Title = "TECHWARE 最好的代碼",
+                Text = "aimlock 不再被激活 (false)"
+            })
+        else
+            drizzy_groomer.activado = true
+            thisaudiowasmadebytueniSEXdrip:SetCore("SendNotification",{
+                Title = "TECHWARE 最好的代碼",
+                Text = "aimlock 活性 (true)"
+            })
+        end
+    end
+end)
+
+-- // momento metamethods
+setreadonly(doxxeados_asesinos,false)
+doxxeados_asesinos.__namecall = newcclosure(function(self,...) 
+	local metodos_de_audios_de_dripin = {...}
+	local creacion_de_audios_loud = getnamecallmethod()
+	if creacion_de_audios_loud == "FireServer" and espedofilo("Drizzy",self) and metodos_de_audios_de_dripin[1] == "UpdateMousePos" and drizzy_groomer["activado"] and drizzy_groomer["victima"] then 
+		metodos_de_audios_de_dripin[2] = groomear("Drizzy",drizzy_groomer["victima"],true,true)
+	end
+	return dahood(self,unpack(metodos_de_audios_de_dripin))
+end)
+setreadonly(doxxeados_asesinos,true)
