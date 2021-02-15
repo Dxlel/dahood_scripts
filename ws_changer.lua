ce4ac212bbcb301ab0a488feddd892e7de5545cb
misamiru <71678108+misamiru@users.noreply.github.com>
2021-02-14 23:07:04 -0300

    Create ws_changer.lua

---
+++ ws_changer.lua
@@ -0,0 +1,155 @@
+--[[
+
+
+                                  ███▄,                              ,╓╖╓,
+                                   ╙█████░▒▄'                      ▄███▒▓▓╣╣╢╗
+                                      ▀▀█████▄,:                  ▐█████████▓▓▓▄
+                                          ` █▓░  █╖               ████████▓▄▓▀██
+                                            '██▄▄█▓▒╗,             ███████▀▀▀██▀
+                                              ▀█████▓▒╢╖            ▀██▄▄░░░░sT
+                                                ██████▓╬╢╖          └███░░░░░─
+                                                 ▀█████▓╣╣▒╣         ███▄µ▒░░
+                                                   ▀████▓▓▓▓╫┐       ,██▀▀
+                                                     ████▓▓█▓▌@▓▒▓▒▄@▓█▓µ▌\  ┌╖
+                                                      ▐███████▓▓▓▓▓▒▄▌╢╢╣▀▒ ,░░░▒▒∩,,
+                                                       ▀███████▓█▓▓╩▒▀█▄▒▒▒▄▒░░░|░▒░░]▒
+                                                          ▀████████▓▓▓▒▀███Ñ▓▒░▒░░░░░▒▒▒
+                                                           ░`▀██████▓▓▓@▒███▒▒▒▒▀▒¢g▄╢▒░
+                                                              ▐█████▓▓▓▓╣▓▒██▌▒▒▒▒▒▒▐███▄
+                                                             ░ ██████▓▓▓▓▌Ñ▓▓▀██▄▒╢¼████æ▄
+                                                               ▀███████▓███▓▓▓▓█░░░░▀▀▀▀██▄
+                                                              ╒█████████▓██▓▓▓▓█▒▒▒▒▒▒▒▒▒░░▄
+                                                             ,▓█╣▓██▓▓███████▓▓█▒▒▒╢╢▒▒▒▒▒▒░
+                                                            ▄▓█▓▄▓███▓█████████████▄▒╢▓╣▓▓╜
+                                                           ▐████▓▓██▓▓▓▓▒▒╢╣╣▒▒▒▀█▀█-└▀▀`
+
+
+
+                                        ▄▄▄█████▓▓█████  ▄████▄   ██░ ██  █     █░ ▄▄▄       ██▀███  ▓█████ 
+                                        ▓  ██▒ ▓▒▓█   ▀ ▒██▀ ▀█  ▓██░ ██▒▓█░ █ ░█░▒████▄    ▓██ ▒ ██▒▓█   ▀ 
+                                        ▒ ▓██░ ▒░▒███   ▒▓█    ▄ ▒██▀▀██░▒█░ █ ░█ ▒██  ▀█▄  ▓██ ░▄█ ▒▒███   
+                                        ░ ▓██▓ ░ ▒▓█  ▄ ▒▓▓▄ ▄██▒░▓█ ░██ ░█░ █ ░█ ░██▄▄▄▄██ ▒██▀▀█▄  ▒▓█  ▄ 
+                                          ▒██▒ ░ ░▒████▒▒ ▓███▀ ░░▓█▒░██▓░░██▒██▓  ▓█   ▓██▒░██▓ ▒██▒░▒████▒
+                                          ▒ ░░   ░░ ▒░ ░░ ░▒ ▒  ░ ▒ ░░▒░▒░ ▓░▒ ▒   ▒▒   ▓▒█░░ ▒▓ ░▒▓░░░ ▒░ ░
+                                            ░     ░ ░  ░  ░  ▒    ▒ ░▒░ ░  ▒ ░ ░    ▒   ▒▒ ░  ░▒ ░ ▒░ ░ ░  ░
+                                          ░         ░   ░         ░  ░░ ░  ░   ░    ░   ▒     ░░   ░    ░   
+                                                    ░  ░░ ░       ░  ░  ░    ░          ░  ░   ░        ░  ░
+                                                        ░                                                   
+
+                                         ▄▄▄▄ ▓██   ██▓    ███▄ ▄███▓ ██▓  ██████  ▄▄▄      ▓█████  ██▓    
+                                        ▓█████▄▒██  ██▒   ▓██▒▀█▀ ██▒▓██▒▒██    ▒ ▒████▄    ▓█   ▀ ▓██▒    
+                                        ▒██▒ ▄██▒██ ██░   ▓██    ▓██░▒██▒░ ▓██▄   ▒██  ▀█▄  ▒███   ▒██░    
+                                        ▒██░█▀  ░ ▐██▓░   ▒██    ▒██ ░██░  ▒   ██▒░██▄▄▄▄██ ▒▓█  ▄ ▒██░    
+                                        ░▓█  ▀█▓░ ██▒▓░   ▒██▒   ░██▒░██░▒██████▒▒ ▓█   ▓██▒░▒████▒░██████▒
+                                        ░▒▓███▀▒ ██▒▒▒    ░ ▒░   ░  ░░▓  ▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░░░ ▒░ ░░ ▒░▓  ░
+                                        ▒░▒   ░▓██ ░▒░    ░  ░      ░ ▒ ░░ ░▒  ░ ░  ▒   ▒▒ ░ ░ ░  ░░ ░ ▒  ░
+                                         ░    ░▒ ▒ ░░     ░      ░    ▒ ░░  ░  ░    ░   ▒      ░     ░ ░   
+                                         ░     ░ ░               ░    ░        ░        ░  ░   ░  ░    ░  ░
+                                              ░░ ░                                                         
+
+
+-- // INFORMACION:
+-- script de aumentar velocidad en da hood (como el de kaias supondre no se a mi no me ip loggearon)
+-- solo funciona en synapse X o sirhurt (posiblemente krnl pero no lo probe)
+-- presiona tecla1 para aumentar +5 la velocidad
+-- presiona tecla2 para reducir -5 la velocidad
+-- presiona tecla3 para desactivar/activar el ws changer
+
+
+-- // CREDITOS:
+-- hecho por cute_misael en roblox/misael#1051 (550687407020965888) en discord
+-- gracias a 2a60/Horbyss (tfw cuando eres pedofilo, te muestran pruebas y aun asi decis que no sos pedofilo plus modulador de voz) por la idea
+
+-- // CREDITOS ADICIONALES: 
+-- doxxeados_asesinos (mininos asesinos)
+-- 4PEDOFILO (4drizzy)
+-- 26Skid (tfw cuando alguien que no muchos conocen hace audios mas fuertes que los tuyos y le decis que usa metodos de otras personas y usas metodos de otros kek)
+-- Skidable19 (tfw cuando intentas cookie loggear)
+-- el que hizo kaias jaja (tremendo ip logger se comieron los q lo usaron)
+-- por ultimo gracias a pedos on top (best on top) y R (lleno de virgos)
+
+-- // NOTAS:
+-- fuck UserInputService all my homies use GetMouse
+-- es open source asi que si usas mi metodo para hacer tu propio walkspeed changer, porfavor dame creditos
+-- this is open source so if you are going to use my method to do your own walkspeed changer, please give credits
+
+]]
+
+-- // servicios
+local MININOS_DOXXEADOS = game:GetService("Players")
+local AUDIOS_LOUD_DE_TRAP = game:GetService("StarterGui") or "son una mierda"
+
+-- // objetos
+local neonazi = MININOS_DOXXEADOS.LocalPlayer
+local esvastica = neonazi:GetMouse()
+
+-- // variables
+local lista_de_victimas_de_drizzy = getrenv()._G
+local da_hood_rblxm_REAL = getrawmetatable(game)
+local CP = da_hood_rblxm_REAL.__newindex
+local CP_DE_DRIZZY = da_hood_rblxm_REAL.__index
+local velocidad_de_cum = 16
+local es_pedofilo = true
+
+-- // funciones para acortar codigo :]
+function anunciar_atentado_terrorista(fecha_del_atentado)
+    AUDIOS_LOUD_DE_TRAP:SetCore("SendNotification",{
+        Title="TECHWARE 最好的代碼",
+        Text=fecha_del_atentado
+    })
+end
+
+-- // evitar que el script se ejecute de nuevo y se bugee todo o evitar errores en otros juegos que no oson da hood
+if game.PlaceId ~= 2788229376 or TECHWAREWALKSPEED_LOADED then
+    anunciar_atentado_terrorista("ESTE JUEGO NO ES DA HOOD/YA EJECUTASTE EL SCRIPT ANTES")
+    return
+end
+getgenv().TECHWAREWALKSPEED_LOADED = true
+
+-- // creditos que si eliminas/editas para darte los creditos a vos mismo probablemente seas una persona triste 
+anunciar_atentado_terrorista("SCRIPT HECHO POR CUTE_MISAEL\nWALKSPEED CHANGER ACTIVADO")
+
+wait(1.5)
+
+anunciar_atentado_terrorista("APRETA LA TECLA "..TeclasWS.tecla1.." PARA AUMENTAR LA VELOCIDAD")
+anunciar_atentado_terrorista("APRETA LA TECLA "..TeclasWS.tecla2.." PARA DISMINUIR LA VELOCIDAD")
+anunciar_atentado_terrorista("APRETA LA TECLA "..TeclasWS.tecla3.." PARA DESACTIVAR/ACTIVAR EL WS CHANGER")
+
+-- // conexión
+esvastica.KeyDown:Connect(function(el_impostor)
+    if el_impostor:lower() == TeclasWS.tecla1:lower() then
+        velocidad_de_cum = velocidad_de_cum + 5
+        anunciar_atentado_terrorista("播放器速度已提高 (speed = "..tostring(velocidad_de_cum)..")")
+    elseif el_impostor:lower() == TeclasWS.tecla2:lower() then
+        velocidad_de_cum = velocidad_de_cum - 5
+        anunciar_atentado_terrorista("玩家的速度已降低 (speed = "..tostring(velocidad_de_cum)..")")
+    elseif el_impostor:lower() == TeclasWS.tecla3:lower() then
+        if es_pedofilo then
+            es_pedofilo = false
+            anunciar_atentado_terrorista("WS Changer 它不再被激活 (false)")
+        else
+            es_pedofilo = true
+            anunciar_atentado_terrorista("WS Changer 已經被激活 (true)")
+        end
+    end
+end)
+
+-- // mi parte favorita: metametodos
+setreadonly(da_hood_rblxm_REAL,false)
+da_hood_rblxm_REAL.__index = newcclosure(function(BEST_ON_TOP,IS_GARBAGE)
+    local esPedofilo = checkcaller()
+    if IS_GARBAGE == "WalkSpeed" and not esPedofilo then
+        return lista_de_victimas_de_drizzy.CurrentWS
+    end
+    return CP_DE_DRIZZY(BEST_ON_TOP,IS_GARBAGE)
+end)
+da_hood_rblxm_REAL.__newindex = newcclosure(function(kaias,ip,logger)
+    local unNeonazi = checkcaller()
+    if es_pedofilo then
+        if ip == "WalkSpeed" and logger ~= 0 and not unNeonazi then
+            return CP(kaias,ip,velocidad_de_cum)
+        end
+    end
+    return CP(kaias,ip,logger)
+end)
+setreadonly(da_hood_rblxm_REAL,true)
